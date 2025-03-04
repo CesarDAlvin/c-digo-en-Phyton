691a3fd80a8da94c59948b6a0ca5d4c28b1a1d02
CesarDAlvin <164974768+CesarDAlvin@users.noreply.github.com>
2025-03-03 20:29:09 -0600

    Ejemplo 2 - F01.py
    Cambios al codigo

---
+++ Ejemplo 2 - F01.py
@@ -0,0 +1,20 @@
+# TEAM 
+
+# ESTE CÓDIGO MUESTRA LA CREACION DE UNA LISTA A QUE LE SE AGREGARA UN NUEVO ELEMENTO Y POSTERIORMENTE SE IMPRIMIRA EL RESULTADO PARA
+# PARA COMPROBAR QUE SE LA ACCION REALIZADA SI SE EJECUTO
+
+# 2025 / 03/ 03 - V. 1. 1. 0
+
+# TRABAJARON: CESAR ARTURO / CesarDAlvin
+
+frutas = ["Uvas", "Guanabana", "Piña", "Fresa"]
+
+
+frutas.append("Mandarina")
+
+
+print("Lista de frutas:", frutas)
+
+
+for fruta in frutas:
+    print("Tengo una", fruta)
